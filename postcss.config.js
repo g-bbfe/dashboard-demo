d7b27bf58b1f2ddefeccc7daa05eda0da49318d0
Oscar-ren <rxl719704258@gmail.com>
2017-08-16 18:23:04 +0800

    config postcss.config.js to support nest

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,9 @@
 module.exports = {
-  plugins: [
-    require('autoprefixer')()
-  ]
-}
+  plugins: {
+    'postcss-cssnext': {
+      "calc": {},
+      "nesting": {},
+      'autoprefixer': {}
+    }
+  }
+};
