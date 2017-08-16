c41a9863d0a7b41f65c8e3f6f9e255c8c3876cf4
Oscar-ren <rxl719704258@gmail.com>
2017-08-16 21:09:25 +0800

    add eslint,babelrc,editorconfig etc config

--- src/www/business/apps/main.js
+++ src/www/business/apps/main.js
@@ -1,41 +1,40 @@
-import Vue from 'vue'
-import ElementUI from 'element-ui'
+import Vue from 'vue';
+import ElementUI from 'element-ui';
 // import '../theme/index.css'
-import App from './App.vue'
-import echarts from 'echarts'
-import Router from 'vue-router'
-import Layout from './home/Layout.vue'
-import Second from './second/Second.vue'
+import App from './App.vue';
+import echarts from 'echarts';
+import Router from 'vue-router';
+import Layout from './home/Layout.vue';
+import Second from './second/Second.vue';
 
-Vue.prototype.$echarts = echarts
+Vue.prototype.$echarts = echarts;
 
-
-Vue.use(ElementUI)
-Vue.use(Router)
+Vue.use(ElementUI);
+Vue.use(Router);
 
 // new Vue({
 //     el: '#app1',
 //     render: h => h(App1)
 // })
 
-const routes = [ //定义路由
+const routes = [ // 定义路由
     { path: '/', component: Layout },
     { path: '/home', component: Layout },
-    {
-        path: '/2',
-        component: Second
+  {
+    path: '/2',
+    component: Second
             //   children:[
             //     {path: '/', component: Submit},
             //     {path: 'submit', component: Submit}
             //   ]
-    }
-]
+  }
+];
 
-const router = new Router({ //创建路由实例
-    routes
-})
+const router = new Router({ // 创建路由实例
+  routes
+});
 
 new Vue({
-    router: router,
-    render: h => h(App)
-}).$mount('#app')
+  router: router,
+  render: h => h(App)
+}).$mount('#app');
