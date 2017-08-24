af2a3692ae5677201120c1c769b4006c928b6680
Oscar-ren <rxl719704258@gmail.com>
2017-08-24 17:04:09 +0800

    upgrade vue-loader to fixed postcss

--- src/www/business/public-apps/navigation/index.vue
+++ src/www/business/public-apps/navigation/index.vue
@@ -59,7 +59,11 @@
 
 <style scope>
   .fade-enter-active, .fade-leave-active {
-    transition: opacity .5s
+    transition : opacity .5s;
+
+    & .hot {
+      background : red;
+    }
   }
   .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
     opacity: 0
